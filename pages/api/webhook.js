f34ea421c3f627cd3927120a8343302fe4216a8f
Gautam327 <rahulpathak43095@gmail.com>
2024-03-28 22:28:20 +0530

    first commit

---
+++ pages/api/webhook.js
@@ -0,0 +1,29 @@
+import {initMongoose} from "../../lib/mongoose";
+import Order from "../../models/Order";
+import {buffer} from 'micro';
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+// localhost:3000/api/webhook
+export default async function handler(req, res) {
+  await initMongoose();
+  const signingSecret = 'whsec_cfaefc13bb474324d5e09470625788cb52c585de525f9213f3bffe37b7e9a634';
+  const payload = await buffer(req);
+  const signature = req.headers['stripe-signature'];
+  const event = stripe.webhooks.constructEvent(payload, signature, signingSecret);
+
+  if (event?.type === 'checkout.session.completed') {
+    const metadata = event.data?.object?.metadata;
+    const paymentStatus = event.data?.object?.payment_status;
+    if (metadata?.orderId && paymentStatus === 'paid') {
+      await Order.findByIdAndUpdate(metadata.orderId, {paid:1});
+    }
+  }
+
+  res.json('ok');
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  }
+};
